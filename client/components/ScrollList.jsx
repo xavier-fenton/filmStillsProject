902d2c48988927c4817150941344f3ab92e6d6e1
Xavier Fenton <xavierfenton99@gmail.com>
2022-09-19 20:26:00 +1200

    filler images

---
+++ client/components/ScrollList.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+
+// const ScrollList = () => {
+//   const data = [
+//     {
+//       movie1: 'jurassic park',
+//     },
+//     {
+//       movie2: 'ground hog days',
+//     },
+//     {
+//       movie3: 'saving private frey',
+//     },
+//     {
+//       movie4: 'toy story: the bully nextdoor',
+//     },
+//   ]
+
+//   return (
+//     {data.map((movie) =>  {
+//       return <ul>{movie.movie1}</ul>
+//     })}
+//   )
+
+// }
+
+// export default ScrollList

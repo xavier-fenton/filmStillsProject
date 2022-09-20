3bd627ac133fc6be78c88869b1f5d8df757248fa
Xavier Fenton <xavierfenton99@gmail.com>
2022-09-20 21:02:42 +1200

    create seamless scrolling, add placeholder film titles

---
+++ client/components/Images.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Images() {
+  return <img className="overflow" src="/images/tak.jpg" alt="tak"></img>
+}
---
+++ client/components/ScrollList.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function ScrollList() {
+  return (
+    <>
+      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/blade.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/blade.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/blade.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
+      <br></br>
+      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+      <br></br>
+    </>
+  )
+}

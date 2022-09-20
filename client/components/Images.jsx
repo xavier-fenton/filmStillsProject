419f0dae3aded09ed3fda901e6340f1191ff6c2a
xavier-fenton <xavierfenton99@gmail.com>
2022-09-21 11:50:25 +1200

    basic button placeholder

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 
+function handleClick() {
+  console.log('hit')
+}
 export default function Images() {
-  return <img className="overflow" src="/images/tak.jpg" alt="tak"></img>
+  return (
+    <>
+      <img className="overflow" src="/images/tak.jpg" alt="tak"></img>
+      <div className="nextButton">
+        <button className="next" onClick={handleClick}>
+          next
+        </button>
+      </div>
+    </>
+  )
 }

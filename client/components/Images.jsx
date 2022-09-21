160e3c9e92abd8ca252506a5a9aa32a6fd65d12b
xavier-fenton <xavierfenton99@gmail.com>
2022-09-21 13:16:10 +1200

    show/hide button

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export default function Images() {
-  return <img className="overflow" src="/images/tak.jpg" alt="tak"></img>
+  return (
+    <>
+      <img className="overflow" src="/images/tak.jpg" alt="tak"></img>
+    </>
+  )
 }
--- client/components/ScrollList.jsx
+++ client/components/ScrollList.jsx
@@ -1,32 +1,43 @@
 import React from 'react'
 
+function showTitles() {
+  const divElement = document.getElementById('titles')
+  if (divElement.style.display === 'none') {
+    divElement.style.display = 'block'
+  } else {
+    divElement.style.display = 'none'
+  }
+}
 export default function ScrollList() {
   return (
     <>
-      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/blade.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/blade.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/blade.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
-      <br></br>
-      <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
-      <br></br>
+      <button onClick={showTitles}>show/hide</button>
+      <div id="titles">
+        <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/blade.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/blade.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/blade.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/midnightmaxi.png" alt="placeholder" />
+        <br></br>
+        <img src="/images/Titles/enterthevoid.png" alt="placeholder" />
+        <br></br>
+      </div>
     </>
   )
 }
